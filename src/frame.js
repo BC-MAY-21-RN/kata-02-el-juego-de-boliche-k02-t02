7083af93f6b65507b1d8c3c5fe3d0a9e7dff3c8a
Shadic78 <carlosjosue7889@gmail.com>
2021-09-01 10:51:15 -0500

    Add functionality for play() method at Frame class
    Co-authored-by: Vladimir-Maidana <maidana.vladi@gmail.com>
    Co-authored-by: EusebioAjas <eusebioajas6@gmail.com>

--- src/frame.js
+++ src/frame.js
@@ -5,51 +5,87 @@ class Frame {
         this.firstTurnPins = 0;
         this.secondTurnPins = 0;
         this.isFirstTurn = true;
-        this.nextFrame = null;
+        this.isFinished = false;
+    }
+
+    customPlay(knockedDownPins) {
+      if(this.isFinished) return;
+      if(this.isFirstTurn) {
+        this.firstTurnPins = knockedDownPins;
+        this.isFirstTurn = false;
+      } else {
+        this.secondTurnPins = knockedDownPins;
+        this.isFinished = true;
+      }
+
+      if(!this.isStrike()) return;
+      this.isFinished = true;
     }
 
     play() {
+      const remainingPins = this.initialPins - this.firstTurnPins;
+      const knockedDownPins = this.getRandom(remainingPins);
+      this.customPlay(knockedDownPins);
        /*
-         ¿Es mi primer turno?
-         -> Derribar pins
-         -> Almacenar pins derribados
-         -> ¿Fue strike?
-         ----> El puntaje es 10 + los siguientes dos turnos + frame anterior
-         ----> Pasar al siguiente frame
-         -> No fue strike:
-         ----> Pasar al turno 2
+        1. Comienza el programa
+        2. Se "ejecuta" el frame 1
+        3. Se realiza una tirada en el frame 1
+        4. { Se realiza el procesamiento del frame }
+        5. Se avanza al siguiente frame y se repite todo
 
-         Es el segundo turno
-         -> Derribar pins
-         -> Almacenar pins derribados
-         -> ¿Fue spare?
-         ----> El puntaje es 10 + el siguiente turno
-         ----> Pasar al siguiente frame
-         -> No fue spare:
-         ----> El puntaje es primer turno + segundo turno + frame anterior
-         ----> Pasar al siguiente frame
+        Problema:
+        - El puntaje
+        Si en el frame n se hizo un strike, entonces no se puede calcular el puntaje en ese
+        momento, el puntaje depende de los siguientes dos frames
+
+        Solución:
+        Iteracion 1:
+        El procesamiento del frame unicamente sea derribar bolos
+        Frame 1 -> Strike -> Se almacena que fue strike y el puntaje no se calcula
+        Frame 2 -> T1: 3, T2: 5 -> Se almacena que se derribaron 8 bolos y se almacena
+        Frame 3 -> Strike
+        Frame 4 -> T1: 4, 
+        .
+        .
+        .
+
+        Iteracion 2:
+        Frame 1 -> Vemos que fue strike entonces el puntaje es 10 + los siguientes dos frames -> 30
+        Frame 2 -> Frame anterior + el frame 2
+        Frame 3 -> Frame anterior + 10 + los siguiente dos
+
+        Necesitamos:
+        - Clase que contenga a los 10 frames y los procese
+
+        frame.play();
+        -> Diferenciar Turno 1, Turno 2
+        -> Si no no se hizo un strike avanzar al turno 2
+        -> Almacenar los bolos derribados
+        -> Debe almacenar su estado (terminado, no terminado)
+        
+        for(recorre la lista de frames) {
+          if(frame.noHaTerminado) {
+            frame.play();
+          }
+        }
+        
        */
-       this.initialPins -= this.Random();
+    }
+
+    isFrameFinished() {
+      return this.isFinished;
     }
 
     isStrike() {
-      return this.firstTurnPins == 10 && this.isFirstTurn;
+      return this.firstTurnPins == 10;
     }
     
     isSpare(){
-      return this.secondTurnPins == 10 && !this.isFirstTurn;
+      return (this.firstTurnPins + this.secondTurnPins) == 10 ;
     }
 
-    setNextFrame(frame) {
-      this.nextFrame = frame;
-    }
-
-    getNextFrame() {
-      return this.nextFrame;
-    }
-
-    getRandom(){
-      return Math.floor(Math.random() * 10);
+    getRandom(max){
+      return Math.floor(Math.random() * (max + 1));
     }
 }
 
